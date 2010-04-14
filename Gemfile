273b75ce684b06cd2c65691423f8c7506150f19f
James Cox <james@imaj.es>
2010-04-14 13:44:37 -0400

    adding in a gemfile, base sinatra app and changes to fields in log entry

---
+++ Gemfile
@@ -0,0 +1,9 @@
+
+## web
+gem 'sinatra'
+gem 'sinatra-mongoid', :require => 'sinatra/mongoid'
+gem 'erb'
+
+## importer
+gem 'nokogiri'
+gem 'httparty'
--- lib/log_entry.rb
+++ lib/log_entry.rb
@@ -2,10 +2,12 @@ require 'mongoid'
 
 class LogEntry
   include Mongoid::Document
+  include Mongoid::Timestamps
   
   field :sender
   field :destination
   field :sent_at, :type => DateTime
   field :message
+  field :network
   
 end
